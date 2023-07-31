3672af4e9da762d807110bf5fe207afffaef1abe
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-07-31 14:03:07 +0300

    name change

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { persistor, store } from "./service/Store";
+import { persistor, store } from "./redux/Store";
 import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(
