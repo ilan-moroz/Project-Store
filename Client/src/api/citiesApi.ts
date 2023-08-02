0a74c30d970aaf8423bab4979e1d8ac9b91f6195
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-08-02 11:53:16 +0300

    filter out empty city names

--- Client/src/api/citiesApi.ts
+++ Client/src/api/citiesApi.ts
@@ -6,9 +6,9 @@ const getCitiesData = async () => {
       "https://data.gov.il/api/3/action/datastore_search?resource_id=5c78e9fa-c2e2-4771-93ff-7f400a12f7ba&q=&limit=9999"
     );
     const data = response.data.result.records;
-    const cityNames = data.map(
-      (city: { שם_ישוב_לועזי: string }) => city.שם_ישוב_לועזי
-    );
+    const cityNames = data
+      .map((city: { שם_ישוב_לועזי: string }) => city.שם_ישוב_לועזי)
+      .filter((cityName: string) => cityName !== " ");
     return cityNames;
   } catch (error) {
     console.error(error);
