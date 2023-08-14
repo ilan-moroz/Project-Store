86a9fe1603d4561e1a5ed72ca140c9f7d3b40c42
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-08-14 13:41:21 +0300

    base card

--- Client/src/components/Cart.tsx
+++ Client/src/components/Cart.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+
 const Cart = () => {
+  const [total, setTotal] = React.useState(0);
+
   return (
     <div style={{ marginTop: "-2rem" }}>
       <h2>Your Cart</h2>
+      <h3>Total: &#8362; {total.toFixed(2)}</h3>
     </div>
   );
 };
