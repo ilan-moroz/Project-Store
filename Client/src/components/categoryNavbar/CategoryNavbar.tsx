613107ae05a09c3d93ecdcbec0214569b23f9b95
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-08-15 13:22:10 +0300

    selected catogry hover and style

--- Client/src/components/categoryNavbar/CategoryNavbar.tsx
+++ Client/src/components/categoryNavbar/CategoryNavbar.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useCategory } from "../../hooks/useCategory";
 import { Category } from "../../models/Category";
 import "./categoryNavbar.css";
@@ -10,13 +11,22 @@ const CategoryNavbar: React.FC<CategoryProps> = ({ onSelectCategory }) => {
   // get all categories from a custom hook
   const catagories = useCategory();
 
+  const [activeCategory, setActiveCategory] = React.useState<string | null>(
+    null
+  );
+
   return (
     <div className="categoryNavbar">
       {catagories.map((category: Category) => (
         <ul
-          className="category"
+          className={`category ${
+            category._id === activeCategory ? "active" : ""
+          }`}
           key={category._id}
-          onClick={() => onSelectCategory(category._id)}
+          onClick={() => {
+            onSelectCategory(category._id);
+            setActiveCategory(category._id);
+          }}
         >
           <li className="category__link purpleText">{category.categoryName}</li>
         </ul>
