cb65480125fc23355f41331890465affc155119d
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-08-15 18:25:42 +0300

    comments

--- Client/src/components/categoryNavbar/CategoryNavbar.tsx
+++ Client/src/components/categoryNavbar/CategoryNavbar.tsx
@@ -11,23 +11,28 @@ const CategoryNavbar: React.FC<CategoryProps> = ({ onSelectCategory }) => {
   // get all categories from a custom hook
   const catagories = useCategory();
 
+  // State to track the currently active category
   const [activeCategory, setActiveCategory] = React.useState<string | null>(
     null
   );
 
   return (
     <div className="categoryNavbar">
+      {/* map all categories to display */}
       {catagories.map((category: Category) => (
         <ul
+          // Apply 'active' class if the category is currently active
           className={`category ${
             category._id === activeCategory ? "active" : ""
           }`}
           key={category._id}
           onClick={() => {
+            // If clicked category is already active, deactivate it
             if (activeCategory === category._id) {
               setActiveCategory(null);
-              onSelectCategory(""); // Or use null, but ensure `handleCategorySelect` in Products component can handle it
+              onSelectCategory("");
             } else {
+              // Otherwise, activate the clicked category
               setActiveCategory(category._id);
               onSelectCategory(category._id);
             }
