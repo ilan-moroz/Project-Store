c3c41ce7e99c626073c144b35e67542f67fde473
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-08-15 19:33:15 +0300

    add product validations

--- Client/src/validators/addProductValidators.ts
+++ Client/src/validators/addProductValidators.ts
@@ -19,6 +19,35 @@ export const resolver: Resolver<AddProductFormValues> = async values => {
     };
   }
 
+  // Check if the Category is empty.
+  if (!values.categoryId || values.categoryId.length === 0) {
+    errors.categoryId = {
+      type: "required",
+      message: "Category name is required",
+    };
+  }
+
+  // Check if the price is empty or 0.
+  if (!values.price) {
+    errors.price = {
+      type: "required",
+      message: "Price is required",
+    };
+  } else if (values.price < 1) {
+    errors.price = {
+      type: "required",
+      message: "Price must be greater than zero",
+    };
+  }
+
+  // Check if the image is empty.
+  if (!values.imagePath || values.imagePath.length === 0) {
+    errors.imagePath = {
+      type: "required",
+      message: "Image is required",
+    };
+  }
+
   //return an object with the valid form values and any errors.
   // If there are any errors, the values are an empty object.
   return {
