5636c194b19174a3a97ecfdcfab9d261c1447fb6
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-08-18 12:11:26 +0300

    product card hover

--- Client/src/components/ProductCard/ProductCard.tsx
+++ Client/src/components/ProductCard/ProductCard.tsx
@@ -50,55 +50,58 @@ const ProductCard: React.FC<cardProps> = ({ product }) => {
   };
 
   return (
-    <Card
-      sx={{
-        width: "12rem",
-        textAlign: "center",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-      }}
-      key={product._id}
-      className="singleCard"
-    >
-      <CardMedia
-        sx={{ height: 100, backgroundSize: "contain" }}
-        image={`http://localhost:4000/${product.imagePath}`}
-        title={product.productName}
-      />
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          sx={{ fontSize: "1rem" }}
-        >
-          {product.productName}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          &#8362; {product.price.toFixed(2)}
-        </Typography>
-        <Box sx={{ marginTop: "0.7rem", marginBottom: "-1.5rem" }}>
-          <NumberInput onValueChange={setQuantity} quantity={quantity} />
-        </Box>
-      </CardContent>
-      <CardActions sx={{ justifyContent: "center" }}>
-        <Button
-          size="small"
-          sx={{ color: "rgb(70,23,155)" }}
-          onClick={() => {
-            addToCart({
-              productId: product._id!,
-              generalPrice: product.price * quantity,
-              quantity: quantity,
-              cartId: cartId,
-            });
-          }}
-        >
-          Add to cart
-        </Button>
-      </CardActions>
-    </Card>
+    <Box className="cardBox">
+      <Card
+        sx={{
+          width: "12rem",
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          height: "17rem",
+        }}
+        key={product._id}
+        className="singleCard"
+      >
+        <CardMedia
+          sx={{ height: 100, backgroundSize: "contain" }}
+          image={`http://localhost:4000/${product.imagePath}`}
+          title={product.productName}
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{ fontSize: "1rem" }}
+          >
+            {product.productName}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            &#8362; {product.price.toFixed(2)}
+          </Typography>
+          <Box sx={{ marginTop: "0.7rem", marginBottom: "-1.5rem" }}>
+            <NumberInput onValueChange={setQuantity} quantity={quantity} />
+          </Box>
+        </CardContent>
+        <CardActions sx={{ justifyContent: "center" }}>
+          <Button
+            size="small"
+            sx={{ color: "rgb(70,23,155)" }}
+            onClick={() => {
+              addToCart({
+                productId: product._id!,
+                generalPrice: product.price * quantity,
+                quantity: quantity,
+                cartId: cartId,
+              });
+            }}
+          >
+            Add to cart
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
   );
 };
 
