4a6ba74412f55f31bcd26be6482cf2bbc00fd727
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-08-21 14:10:35 +0300

    cartId

--- Client/src/hooks/useCartState.ts
+++ Client/src/hooks/useCartState.ts
@@ -4,7 +4,7 @@ import { RootState } from "../redux/Store";
 export const useCartState = () => {
   const { cartId, cartItems, finishedOrder } = useSelector(
     (state: RootState) => ({
-      cartId: state.shoppingCart.cart?._id,
+      cartId: state.shoppingCart.cart?._id ?? "",
       cartItems: state.shoppingCart.cartItems,
       finishedOrder: state.shoppingCart.finishedOrder,
     })
