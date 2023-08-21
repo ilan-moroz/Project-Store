ea11c6f9e24847ecf84b28265cc64f2a33564ea6
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-08-21 14:09:29 +0300

    change useState strecture

--- Client/src/hooks/useCartState.ts
+++ Client/src/hooks/useCartState.ts
@@ -2,18 +2,12 @@ import { useSelector } from "react-redux";
 import { RootState } from "../redux/Store";
 
 export const useCartState = () => {
-  // Get cartId from the Redux store
-  const cartId =
-    useSelector((state: RootState) => state.shoppingCart.cart?._id) ?? "";
-
-  // Get cartItems from the Redux store
-  const cartItems = useSelector(
-    (state: RootState) => state.shoppingCart.cartItems
-  );
-
-  // Get cartItems from the Redux store
-  const finishedOrder = useSelector(
-    (state: RootState) => state.shoppingCart.finishedOrder
+  const { cartId, cartItems, finishedOrder } = useSelector(
+    (state: RootState) => ({
+      cartId: state.shoppingCart.cart?._id,
+      cartItems: state.shoppingCart.cartItems,
+      finishedOrder: state.shoppingCart.finishedOrder,
+    })
   );
 
   return { cartId, cartItems, finishedOrder };
