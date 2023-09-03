6c9313bd04ee82a00604acf5b3458c55c741a57c
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-03 14:40:42 +0300

    responsive

--- Client/src/pages/shopping/Shopping.tsx
+++ Client/src/pages/shopping/Shopping.tsx
@@ -1,7 +1,6 @@
 import Cart from "../../components/Cart/Cart";
 import Products from "../../components/Products/Products";
 import "./shopping.css";
-import { Resizable } from "re-resizable";
 import AddProduct from "../../components/AddProduct";
 import { useUserState } from "../../hooks/useUserState";
 import { useCartState } from "../../hooks/useCartState";
@@ -13,19 +12,10 @@ const Shopping = () => {
 
   return (
     <div className="shopping">
-      {/* resizable comp */}
-      <Resizable
-        defaultSize={{
-          width: 450,
-          height: "100%",
-        }}
-        minWidth={125}
-      >
-        <div className="cartOrAddProduct">
-          {/* if admin show add product comp if user show cart comp */}
-          {user?.role === "admin" ? <AddProduct /> : <Cart />}
-        </div>
-      </Resizable>
+      <div className="cartOrAddProduct">
+        {/* if admin show add product comp if user show cart comp */}
+        {user?.role === "admin" ? <AddProduct /> : <Cart />}
+      </div>
       <div className="shopping__main">
         {!finishedOrder ? <Products /> : <OrderForm />}
       </div>
