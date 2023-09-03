c3f79f27947674f232f4fa430505968b401219f5
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-03 15:04:54 +0300

    add response size

--- Client/src/hooks/useResponsive.ts
+++ Client/src/hooks/useResponsive.ts
@@ -2,6 +2,7 @@ import { useMediaQuery } from "react-responsive";
 
 const useResponsive = () => {
   const isSmallDesktop = useMediaQuery({ query: "(max-width: 87.5em)" }); //1400px
+  const isXsDesktop = useMediaQuery({ query: "(max-width: 65.62em)" }); //1050px
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 50em)" }); //800px
   const isSmallScreen = useMediaQuery({ query: "(max-width: 40.62em)" }); //650px
   const isXsScreen = useMediaQuery({ query: "(max-width: 37.5em)" }); //600px
@@ -11,6 +12,7 @@ const useResponsive = () => {
     isXsScreen,
     isSmallScreen,
     isSmallDesktop,
+    isXsDesktop,
   };
 };
 
