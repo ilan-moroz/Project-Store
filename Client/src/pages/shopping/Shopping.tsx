f8a4602b9c03e46dbb3752b5b6f14126f5c3cc31
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-03 15:05:04 +0300

    create ref for the cart

--- Client/src/pages/shopping/Shopping.tsx
+++ Client/src/pages/shopping/Shopping.tsx
@@ -5,19 +5,22 @@ import AddProduct from "../../components/AddProduct";
 import { useUserState } from "../../hooks/useUserState";
 import { useCartState } from "../../hooks/useCartState";
 import OrderForm from "../../components/OrderForm/OrderForm";
+import React from "react";
 
 const Shopping = () => {
   const { user } = useUserState();
   const { finishedOrder } = useCartState();
 
+  const cartRef = React.useRef(null);
+
   return (
     <div className="shopping">
-      <div className="cartOrAddProduct">
+      <div className="cartOrAddProduct" ref={cartRef}>
         {/* if admin show add product comp if user show cart comp */}
         {user?.role === "admin" ? <AddProduct /> : <Cart />}
       </div>
       <div className="shopping__main">
-        {!finishedOrder ? <Products /> : <OrderForm />}
+        {!finishedOrder ? <Products cartRef={cartRef} /> : <OrderForm />}
       </div>
     </div>
   );
