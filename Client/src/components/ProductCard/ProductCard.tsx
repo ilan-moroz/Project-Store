83f641596c2b7a3c89d0a930438c0d8107a7a1ae
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-05 11:56:52 +0300

    responsive theme

--- Client/src/components/ProductCard/ProductCard.tsx
+++ Client/src/components/ProductCard/ProductCard.tsx
@@ -10,7 +10,7 @@ import { addItemToCartApi } from "../../api/cartApi";
 import { useDispatch } from "react-redux";
 import { CartItem } from "../../models/CartItem";
 import { Product } from "../../models/Product";
-import { Box } from "@mui/material";
+import { Box, ThemeProvider, createTheme } from "@mui/material";
 import { useCartState } from "../../hooks/useCartState";
 import { addItemToCart, updateCartItem } from "../../redux/cartSlice";
 import "./productCard.css";
@@ -59,93 +59,108 @@ const ProductCard: React.FC<cardProps> = ({ product }) => {
     dispatch(productToEdit(product));
   };
 
+  // theme for responsive breakpoints
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 900,
+        lg: 1200,
+        xl: 1900,
+      },
+    },
+  });
+
   return (
-    <Box className="cardBox">
-      <Card
-        sx={{
-          width: { xs: "12rem", xl: "18rem" }, // Change width based on screen size
-          textAlign: "center",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-          height: { xs: "18rem", lg: "18rem", xl: "24rem" },
-          boxShadow: "0px 5px 10px rgba(0,0,0,0.1)",
-        }}
-        key={product._id}
-        className="singleCard"
-      >
-        {isAdmin && (
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "flex-end",
-              padding: "0.5rem",
-            }}
-          >
-            <IconButton
-              aria-label="edit"
-              sx={{ width: "2rem" }}
-              onClick={handleEditProduct}
+    <ThemeProvider theme={theme}>
+      <Box className="cardBox">
+        <Card
+          sx={{
+            width: { xs: "12rem", xl: "17rem" }, // Change width based on screen size
+            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: { xs: "18rem", lg: "18rem", xl: "24rem" },
+            boxShadow: "0px 5px 10px rgba(0,0,0,0.1)",
+          }}
+          key={product._id}
+          className="singleCard"
+        >
+          {isAdmin && (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                padding: "0.5rem",
+              }}
             >
-              <EditIcon />
-            </IconButton>
-          </Box>
-        )}
-        <CardMedia
-          sx={{ height: 150, backgroundSize: "contain" }}
-          image={`http://localhost:4000/${product.imagePath}`}
-          title={product.productName}
-        />
-        <CardContent>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            sx={{ fontSize: "1.2rem", fontWeight: "500" }}
-          >
-            {product.productName}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            sx={{ fontWeight: "bold" }}
-          >
-            &#8362; {product.price.toFixed(2)}
-          </Typography>
-          {!isAdmin && (
-            <Box sx={{ marginTop: "1rem", marginBottom: "-1rem" }}>
-              <NumberInput onValueChange={setQuantity} quantity={quantity} />
+              <IconButton
+                aria-label="edit"
+                sx={{ width: "2rem" }}
+                onClick={handleEditProduct}
+              >
+                <EditIcon />
+              </IconButton>
             </Box>
           )}
-        </CardContent>
-        {!isAdmin && (
-          <CardActions sx={{ justifyContent: "center" }}>
-            <Button
-              size="medium"
-              variant="outlined"
-              sx={{
-                color: "rgb(103, 32, 180)",
-                borderColor: "rgb(103, 32, 180)",
-                "&:hover": {
-                  backgroundColor: "rgb(103, 32, 180)",
-                  color: "white",
-                },
-              }}
-              onClick={() => {
-                addToCart({
-                  productId: product._id!,
-                  generalPrice: product.price * quantity,
-                  quantity: quantity,
-                  cartId: cartId,
-                });
-              }}
+          <CardMedia
+            sx={{ height: 150, backgroundSize: "contain" }}
+            image={`http://localhost:4000/${product.imagePath}`}
+            title={product.productName}
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{ fontSize: "1.2rem", fontWeight: "500" }}
             >
-              Add to cart
-            </Button>
-          </CardActions>
-        )}
-      </Card>
-    </Box>
+              {product.productName}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ fontWeight: "bold" }}
+            >
+              &#8362; {product.price.toFixed(2)}
+            </Typography>
+            {!isAdmin && (
+              <Box sx={{ marginTop: "1rem", marginBottom: "-1rem" }}>
+                <NumberInput onValueChange={setQuantity} quantity={quantity} />
+              </Box>
+            )}
+          </CardContent>
+          {!isAdmin && (
+            <CardActions sx={{ justifyContent: "center" }}>
+              <Button
+                size="medium"
+                variant="outlined"
+                sx={{
+                  color: "rgb(103, 32, 180)",
+                  borderColor: "rgb(103, 32, 180)",
+                  "&:hover": {
+                    backgroundColor: "rgb(103, 32, 180)",
+                    color: "white",
+                  },
+                }}
+                onClick={() => {
+                  addToCart({
+                    productId: product._id!,
+                    generalPrice: product.price * quantity,
+                    quantity: quantity,
+                    cartId: cartId,
+                  });
+                }}
+              >
+                Add to cart
+              </Button>
+            </CardActions>
+          )}
+        </Card>
+      </Box>
+    </ThemeProvider>
   );
 };
 
